ad390037ec690193620a9fe55101ca4f54144183
yaki-toki <rlawlgk23@gmail.com>
2023-01-03 13:00:02 +0900

    Modifing code

--- ChangeCode.c
+++ ChangeCode.c
@@ -18,6 +18,7 @@ typedef struct  {
     int nowBackoff;
     int firstTxTime;
     int secondTxTime;
+    int RaFailed;
     int RA;
 
     int power;
@@ -29,12 +30,12 @@ int backoffIndicator = 20; // Number of backoff indicator
 int nGrantUL = 12;         // Number of UL Grant
 
 int maxRarWindow = 5;
-int maxMsg1ReTx = 10;
+int maxMsg1ReTx = 9;
 int accessTime = 5;
 
-void initUserInfo(UserInfo *user, int idx, int timer, int active, int preamble, int rarWindow, int msg1ReTx, int msg2, int msg3Wait, int txTime, int nowBackoff, int RA, int power, double channelGain){
+void initUserInfo(UserInfo *user, int idx, int timer, int active, int preamble, int rarWindow, int msg1ReTx, int msg2, int msg3Wait, int txTime, int nowBackoff, int RaFailed, int RA, int power, double channelGain){
     user->idx = idx;
-    user->timer = timer;
+    user->timer = 0;
     user->active = active;
     user->preamble = preamble;
     user->rarWindow = rarWindow;
@@ -43,10 +44,22 @@ void initUserInfo(UserInfo *user, int idx, int timer, int active, int preamble, 
     user->msg3Wait = msg3Wait;
     user->txTime = txTime;
     user->nowBackoff = nowBackoff;
+    user->RaFailed = RaFailed;
     user->RA = RA;
 
     user->power = power;
     user->channelGain = channelGain;
+}
+
+void activeUE(UserInfo *user, int time){
+    user->active = 1;
+    user->preamble = rand() % nPreamble;
+    user->txTime = time + 1;
+    user->timer = 0;
+    user->rarWindow = 0;
+    user->msg1ReTx = 0;
+    user->nowBackoff = 0;
+    user->firstTxTime = time + 1;
 }
 
 void preambleCollisionDetection(UserInfo *user, int activeCheck, int time, int *grantCheck){
@@ -55,13 +68,14 @@ void preambleCollisionDetection(UserInfo *user, int activeCheck, int time, int *
 
     // Store idx of UEs attempting to send MSG 1 at the current time
     for(int i = 0; i < activeCheck; i++){
-        if((user + i)->RA == 0 && (user+i)->active == 1 && (user+i)->txTime == time && (user+i)->nowBackoff <= 0 && (user+i)->msg2 == 0){
-            preambleTxUEs[preambleTxCheck] = (user+i)->idx;
-            preambleTxCheck++;
+        if((user + i)->RA == 0 && (user+i)->txTime == time+1 && (user+i)->nowBackoff <= 0 && (user+i)->RaFailed == 0){
+            if((user+i)->active == 1 && (user+i)->msg2 == 0){
+                preambleTxUEs[preambleTxCheck] = (user+i)->idx;
+                preambleTxCheck++;
+            }
         }
     }
-
-    if(preambleTxCheck != 0){
+    if(preambleTxCheck > 0){
         int *preambles = (int*)calloc(nPreamble, sizeof(int));
         int **userCheck = (int**)malloc(nPreamble * sizeof(int*));
         
@@ -82,20 +96,14 @@ void preambleCollisionDetection(UserInfo *user, int activeCheck, int time, int *
                 for(int j = 0; j < preambles[i]; j++){
                     if(*grantCheck <= nGrantUL){
                         (user+userCheck[i][j])->msg2 = 1;
-                        // (user+userCheck[i][j])->txTime += 11;
-                    }else{
-                        (user+userCheck[i][j])->txTime += 3;
                     }
                 }
-                
-            }else if(preambles[i] > 1){
-                // printf("---------\n");
-                for(int j = 0; j < preambles[i]; j++){
-                    // printf("%d %d %d\n", i, preambles[i], userCheck[i][j]);
-                    (user+userCheck[i][j])->txTime += 3;
-                }
             }
+            // else if(preambles[i] > 1){
+            //     printf("%d %d %d| ", time, i, preambles[i]);
+            // }
         }
+            // printf("\n");
         free(preambles);
         for(int i = 0; i < nPreamble; i++){
             free(userCheck[i]);
@@ -106,84 +114,69 @@ void preambleCollisionDetection(UserInfo *user, int activeCheck, int time, int *
 }
 
 void msg2Results(UserInfo *user, int time){
-    int txLimit = 0;
-    if(user->active == 1 && user->nowBackoff <= 0){
-        user->rarWindow++;
-        if(user->msg2 == 0){
-            int randomBackoff = rand() % 4;
+    int txLimit = 1;
+    
+    if(user->msg2 == 0 && user->active == 1){
+        
+        user->rarWindow = 5;
+        user->txTime += 3;
+        if(user->rarWindow >= maxRarWindow){
+            user->rarWindow = 0;
+            user->msg1ReTx++;
+            int tmp = rand() % 4;
+            int value = 0;
 
-            if(randomBackoff == 0){
-                user->txTime += (5-txLimit);
-                user->nowBackoff = user->txTime - time;
-            }else if(randomBackoff == 1){
-                user->txTime += (10-txLimit);
-                user->nowBackoff = user->txTime - time;
-            }else if(randomBackoff == 2){
-                user->txTime += (15-txLimit);
-                user->nowBackoff = user->txTime - time;
-            }else{
-                user->txTime += (20-txLimit);
-                user->nowBackoff = user->txTime - time;
+            if(tmp == 0){
+                user->txTime = time + 5 + txLimit;
+            }else if (tmp == 1){
+                user->txTime = time + 10 + txLimit;
+            }else if (tmp == 2){
+                user->txTime = time + 15 + txLimit;
+            }else if (tmp == 3){
+                user->txTime = time + 20 + txLimit;
             }
+
             user->secondTxTime = user->txTime;
 
-            // printf("%d\n", user->txTime);
-            if(user->rarWindow > maxRarWindow){
-                user->msg1ReTx++;
-                if(user->msg1ReTx == maxMsg1ReTx){
-                    initUserInfo(user, user->idx, 0, 1, rand()%nPreamble, 0, 0, 0, 0, user->txTime, user->nowBackoff, 0, 0, 0.);
-                }
+            if(user->msg1ReTx >= maxMsg1ReTx){
+                user->RaFailed++;
+                user->preamble = rand() % nPreamble;
+                user->rarWindow = 0;
+                user->msg1ReTx = 0;
+                user->nowBackoff = 0;
+                user->timer = 0;
             }
-            
-        }else if(user->msg2 == 1){
-            // MSG 1에 대한 MSG 2수신 성공
-            user->txTime += 11;
         }
+    }else if(user->msg2 == 1){
+        user->active = 2;
+        // MSG 1에 대한 MSG 2수신 성공
+        user->txTime += 10;
+        user->secondTxTime = user->txTime;
+        user->msg3Wait = 0;
     }
-    
 }
 
-/*
-UserInfo *user, int idx, int timer, int active, int preamble, int rarWindow, int msg1ReTx, int msg2, int msg3Wait, int txTime, int nowBackoff, int RA, int power, double channelGain
-*/
-
-void resourceRequestAllocation(UserInfo *user, int activeCheck){
+void resourceRequestAllocation(UserInfo *user, int activeCheck, int time){
     for(int i = 0; i < activeCheck; i++){
-        if((user+i)->msg2 == 1 && (user+i)->txTime && (user+i)->active == 1){
-            if((user->msg3Wait) < 48){
+        if((user+i)->txTime == time && (user+i)->msg2 == 1 && (user+i)->active == 2 && (user+i)->RaFailed == 0){
+            if((user->msg3Wait) <= 48){
                 float p = (float)rand() / (float)RAND_MAX;
                 if(p > 0.1){
                     (user+i)->active = 0;
                     (user+i)->RA = 1;
                     (user+i)->timer = (user+i)->timer + 6;
                 }else{
-                    (user+i)->msg2 = 0;
                     (user+i)->txTime += 48;
                     (user->msg3Wait) = 48;
                 }
             }else{
-                initUserInfo(user, user->idx, 0, 1, rand()%nPreamble, 0, 0, 0, 0, user->txTime, user->nowBackoff, 0, 0, 0.);
+                (user+i)->RaFailed++;
+                int leastTime = time % 5;
+                initUserInfo((user+i), (user+i)->idx, 0, 1, rand() % nPreamble, 0, 0, 0, 0, time + leastTime+1, leastTime+1, (user+i)->RaFailed, 0, 0, 0.);
+                
             }
         }
     }
-}
-/*
-    user->idx = idx;
-    user->timer = timer;
-    user->active = active;
-    user->preamble = preamble;
-    user->rarWindow = rarWindow;
-    user->msg1ReTx = msg1ReTx;
-    user->msg2 = msg2;
-    user->txTime = txTime;
-    user->nowBackoff = nowBackoff;
-    user->RA = RA;
-*/
-void activeUE(UserInfo *user, int time){
-    user->active = 1;
-    user->preamble = rand() % nPreamble;
-    user->txTime = time + 1;
-    user->firstTxTime = time + 1;
 }
 
 void timerIncrease(UserInfo *user){
@@ -219,7 +212,7 @@ void saveResult(UserInfo *user, int nUE){
     fp_l = fopen(fileNameResultLog, "w+");
 
     for(int i = 0; i < nUE; i++){
-        sprintf(logBuff, "Idx: %d| FirstTxTime: %d| SecondTxTime: %d| Timer: %d| Active: %d| Preamble: %d| RarWindow: %d| Msg1ReTx: %d| MSG2: %d| TxTime: %d| Backoff: %d| RA: %d\n",
+        sprintf(logBuff, "Idx: %d| FirstTxTime: %d| SecondTxTime: %d| Timer: %d| Active: %d| Preamble: %d| RarWindow: %d| Msg1ReTx: %d| MSG2: %d| TxTime: %d| Backoff: %d| RAFailed: %d| RA: %d\n",
                         (user + i)->idx, 
                         (user + i)->firstTxTime, 
                         (user + i)->secondTxTime, 
@@ -230,7 +223,8 @@ void saveResult(UserInfo *user, int nUE){
                         (user + i)->msg1ReTx, 
                         (user + i)->msg2, 
                         (user + i)->txTime, 
-                        (user + i)->nowBackoff, 
+                        (user + i)->nowBackoff,
+                        (user + i)->RaFailed,
                         (user + i)->RA);
         fputs(logBuff, fp_l);
     }
@@ -239,13 +233,14 @@ void saveResult(UserInfo *user, int nUE){
 }
 
 int main(int argc, char **argv){
+    srand(1);
     int nUE;
     for(nUE = 10000; nUE <= 100000; nUE += 10000){
         UserInfo *UEs;
         UEs = (UserInfo *)calloc(nUE, sizeof(UserInfo));
 
         for(int i = 0; i < nUE; i++){
-            initUserInfo(UEs+i, i, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.);
+            initUserInfo(UEs+i, i, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.);
         }
 
         int activeCheck = 0;
@@ -256,7 +251,7 @@ int main(int argc, char **argv){
 
         for(time = 0; time < maxTime; time++){
             nSuccessUE = 0;
-            if(time % 5 == 1){
+            if(time % 5 == 0){
                 grantCheck = 0;
             }
 
@@ -268,41 +263,26 @@ int main(int argc, char **argv){
                 if(activeCheck >= nUE){
                     activeCheck = nUE;
                 }
-
                 for(int i = 0; i < activeCheck; i++){
                     if((UEs + i)->RA == 0 && (UEs + i)->active == 0){
                         activeUE(UEs+i, time);
                     }
                 }
+                preambleCollisionDetection(UEs, activeCheck, time, &grantCheck);
+
+                for(int i = 0; i < activeCheck; i++){
+                    if((UEs+i)->nowBackoff <= 0 && (UEs+i)->txTime == time+1 && (UEs+i)->active == 1 && (UEs+i)->RA == 0 && (UEs+i)->RaFailed == 0){
+                        // printf("Here? ");
+                        msg2Results(UEs+i, time);
+                    }
+                }
             }
-
-            // int check = 0;
-            // while(1){
-            //     int randUE = rand() % nUE;
-            //     activeUE(UEs+randUE, time);
-            //     for(int i = 0; i < nUE; i++){
-            //         if((UEs + i)->active == 1){
-            //             check++;
-            //         }
-            //     }
-
-            //     if(check >= activeCheck){
-            //         break;
-            //     }
-            // }
-
-            preambleCollisionDetection(UEs, activeCheck, time, &grantCheck);
-
-            for(int i = 0; i < activeCheck; i++){
-                if((UEs+i)->nowBackoff == 0 && (UEs+i)->active == 1)
-                    msg2Results(UEs+i, time);
-            }
-
-            resourceRequestAllocation(UEs, activeCheck);
+            
+            resourceRequestAllocation(UEs, activeCheck, time);
 
             // Increase timers of all active UEs by 1
-            for(int i = 0; i <= activeCheck; i++){
-                if((UEs+i)->active == 1){
+            for(int i = 0; i < activeCheck; i++){
+                if((UEs+i)->active > 0 && (UEs+i)->RA == 0 && (UEs+i)->RaFailed == 0){
                     timerIncrease(UEs+i);
                 }
             }
@@ -314,7 +294,7 @@ int main(int argc, char **argv){
         saveResult(UEs, nUE);
 
         printf("UE: %d\n", nUE);
-
+        printf("Number of succeed UEs: %d\n", nSuccessUE);
         printf("Success ratio: %lf\n", ((float)nSuccessUE/(float)nUE)*100.0);
         int delay = 0;
         for(int i = 0; i < nUE; i++){
